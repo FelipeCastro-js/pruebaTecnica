52b53b7fad9c97e8bf2ffe5cbb07cd74e086de5a
Felipe Castro <carloscastro1860@gmail.com>
2023-04-03 11:27:34 -0600

    Carpeta de la solucion del punto 3

---
+++ domain-detector/domain-detector/domain-detector/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../", "utils", "example.txt");
+
+
+        try
+        {
+            string contenido = File.ReadAllText(path);
+            Regex regex = new Regex(@"\b(?:http?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(contenido);
+            string urls = "";
+            foreach (Match match in matches)
+            {
+                string url = match.Value;
+                Uri uri = new Uri(url);
+                string dominio = uri.Host;
+
+                dominio = Regex.Replace(dominio, @"^[^\.]*\.", "");
+                urls += dominio + ";"; 
+            }
+
+            Console.WriteLine(urls);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocurrió un error al leer el archivo: " + ex.Message);
+        }
+    }
+}
